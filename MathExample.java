34a38d857cb23580d48adee0666995269fe5003b
Alexander1793 <72270526+Alexander1793@users.noreply.github.com>
2020-10-03 13:35:33 +0300

    Add files via upload

---
+++ MathExample.java
@@ -0,0 +1,37 @@
+package com.company;
+
+public class MathExample {
+
+    public static void Resalt(int a, int b, String x, boolean y)
+    {
+        int resalt = 0;
+        String result_roman;
+        if (x.equals("+") == true)
+            resalt = a + b;
+        if (x.equals("-") == true)
+            resalt = a - b;
+        if (x.equals("*") == true)
+            resalt = a * b;
+        if (x.equals("/") == true)
+        {
+            if (b == 0)
+            {
+                System.out.println("Нельзя делить на ноль");
+                return;
+            }
+            else
+                resalt = a / b;
+        }
+        if (resalt == 0 || resalt > 3999)
+        {
+            System.out.println(resalt + " Невозможно отобразить римскими числами");
+            return;
+        }
+        if ( y == true )
+        {
+            result_roman = Conversion.Conversion_to_roman_num(resalt);
+            System.out.println(result_roman);
+        }
+        else System.out.println(resalt);
+    }
+}
